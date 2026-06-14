68dacc44c64d106ac6ce851e846f76c8da4b738c
RUBisco0211 <1205456072@qq.com>
2026-06-14 13:06:02 +0800

    remove daily limit

--- TexoOCR/AppState.swift
+++ TexoOCR/AppState.swift
@@ -183,13 +183,6 @@ class AppState: ObservableObject {
     func processImage(_ image: NSImage) {
         guard let engine = ocrEngine, !isProcessing else { return }
 
-        // Check daily usage limit
-        let store = StoreManager.shared
-        guard store.canUse else {
-            status = .error(L.dailyLimitReached)
-            return
-        }
-
         let imageData = image.tiffRepresentation
         isProcessing = true
         status = .recognizing
@@ -205,7 +198,6 @@ class AppState: ObservableObject {
                 self.lastResult = latex
                 self.isProcessing = false
                 self.status = .ready
-                store.recordUsage()
 
                 let item = HistoryItem(latex: latex, imageData: imageData)
                 self.history.insert(item, at: 0)
